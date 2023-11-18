af13f46800c671ec0e211227d021545a91123465
sid24desai <sid24desai@gmail.com>
2023-11-18 14:09:10 +0530

    Add project files.

---
+++ WebJobExample.WebJob/Functions.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace WebJobExample.WebJob
+{
+    public class ProcessMessageFunction
+    {
+        private QueueClient _queueClient;
+        public ProcessMessageFunction(IConfiguration configuration)
+        {
+            _queueClient = new QueueClient(configuration["AzureWebJobsStorage"], "processed");
+        }
+
+        public async Task ProcessQueueMessage([QueueTrigger("queue")] string message, ILogger logger)
+        {
+            await _queueClient.SendMessageAsync($"Processed {message} {DateTime.Now}");
+            logger.LogInformation(message);
+        }
+    }
+}
---
+++ WebJobExample.WebJob/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobExample.WebJob
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+#if DEBUG
+            builder.UseEnvironment("development");
+#endif
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
